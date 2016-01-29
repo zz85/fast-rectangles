3591c2dfbe3d51d74157f8ee1b62a8282f0734a1
Joshua Koo <zz85nus@gmail.com>
2016-01-29 09:59:14 +0800

    Nicer test results output

--- boilerplate.js
+++ boilerplate.js
@@ -6,8 +6,17 @@ var timeStart;
 var total = 0;
 var rafs = 0;
 var rects = 0;
+var results = document.createElement('pre');
+results.style.cssText = 'position: fixed; top: 10px; left: 10px; font-size: 10px; z-index: 999; background: rgba(255, 255, 255, 0.5);';
 
 /* Implement your draw function !! */
+
+function log() {
+	console.log.apply(console, arguments);
+
+	results.innerHTML += Array.from(arguments).join('\t') + '\n';
+}
+
 
 function run() {
 	rafs++
@@ -16,26 +25,28 @@ function run() {
 	var now = performance.now();
 	var lapsed = now - timeStart;
 	total += now - then;
+	results.innerHTML += '.';
 
 	if (lapsed < 5000) {
 		requestAnimationFrame(run);
 	} else {
 		console.timeEnd('run');
-		console.log('Number of Rafs', rafs);
-		console.log('Time per Rafs', (total / rafs).toFixed(2), 'ms');
+		log('\nNumber of Rafs', rafs);
+		log('Time per Rafs', (total / rafs).toFixed(2), 'ms');
 
-		console.log('Total Rects', rects);
-		console.log('Rects / second', (rects / total * 1000).toFixed(2));
+		log('Total Rects', rects);
+		log('Rects / second', (rects / total * 1000).toFixed(2));
 
-		console.log('Rects / 16.67ms', (rects * 1000  / total / 60).toFixed(2));
-		console.log('Done!!');
+		log('Rects / 16.67ms', (rects * 1000  / total / 60).toFixed(2));
+		log('Done!!');
 	}
 }
 
 function start() {
-	console.log('Test start!');
+	document.body.appendChild(results);
+
+	log('Test started!');
 	timeStart = performance.now();
 	console.time('run');
-
 	run();
 }
